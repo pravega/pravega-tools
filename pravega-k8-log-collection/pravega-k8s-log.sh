fb26778beef59737e52f548607b635d21aa7e8a2
root <root@manager1.lss.emc.com>
2018-12-11 15:20:14 -0500

    include review changes

--- pravega-k8-log-collection/pravega-k8s-log.sh
+++ pravega-k8-log-collection/pravega-k8s-log.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#/usr/bin/env bash
+set -euo pipefail
 mkdir -p /tmp/logdir
 output_dir="/tmp/logdir"
 kubectl get po,svc,pvc --all-namespaces > $output_dir/kubectl-get-po-svc-pvc-all-namespace_output
@@ -6,18 +7,15 @@ IFS=$'\n'
 for i in $(cat $output_dir/kubectl-get-po-svc-pvc-all-namespace_output | grep -i pravega)
 do
 echo $i
-if [[ $i == *"po"* ]];
-then 
-    if [ `echo $i | awk '{print $5}'` != 0 ]
-        then     
-            kubectl -n `echo $i | awk '{print $1}'` logs `echo $i | awk '{print $2}'` -p > $output_dir/log_`echo $i | awk 'gsub(/\//,"_") {print $2}'`_previous.log
+if [[ $i == *"po/"* ]] || [[ $i == *"pod/"* ]]; then
+    if [ `echo $i | awk '{print $5}'` != 0 ]; then     
+        kubectl -n `echo $i | awk '{print $1}'` logs `echo $i | awk '{print $2}'` -p > $output_dir/log_`echo $i | awk 'gsub(/\//,"_") {print $2}'`_previous.log
     fi
     kubectl -n `echo $i | awk '{print $1}'` logs `echo $i | awk '{print $2}'` > $output_dir/log_`echo $i | awk 'gsub(/\//,"_") {print $2}'`_recent.log
-    kubectl -n `echo $i | awk '{print $1}'` exec -it `echo $i |  awk '-F'[/] '{print $2}'|awk '{print $1}'` -- ls /opt/pravega/logs > /dev/null 2>&1
-    if [ $? == 0 ]; 
-        then
-            mkdir -p $output_dir/oldlogs_`echo $i |  awk '-F'[/] '{print $2}'|awk '{print $1}'` 
-            kubectl -n `echo $i | awk '{print $1}'` cp `echo $i |  awk '-F'[/] '{print $2}'|awk '{print $1}'`:/opt/pravega/logs $output_dir/oldlogs_`echo $i |  awk '-F'[/] '{print $2}'|awk '{print $1}'`/.
+    has_logs=$(kubectl -n `echo $i | awk '{print $1}'` exec -it `echo $i |  awk '-F'[/] '{print $2}'|awk '{print $1}'` -- ls -l /opt/pravega/logs 2>/dev/null | wc -l || echo "")
+    if [ $has_logs -ge 2 ]; then  
+        mkdir -p $output_dir/oldlogs_`echo $i |  awk '-F'[/] '{print $2}'|awk '{print $1}'` 
+        kubectl -n `echo $i | awk '{print $1}'` cp `echo $i |  awk '-F'[/] '{print $2}'|awk '{print $1}'`:/opt/pravega/logs $output_dir/oldlogs_`echo $i |  awk '-F'[/] '{print $2}'|awk '{print $1}'`/.
     fi    
 fi
 kubectl -n `echo $i | awk '{print $1}'` describe `echo $i | awk '{print $2}'` > $output_dir/describe_`echo $i | awk 'gsub(/\//,"_") {print $2}'`_output
