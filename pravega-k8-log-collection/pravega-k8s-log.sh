5c7fa3ef3bec2094f9e2bcdeed6307eba302cc49
Sumit Basu Mallick <sumit.basumallick@emc.com>
2018-12-12 08:29:48 -0500

    including review comments and runnig pod detection

--- pravega-k8-log-collection/pravega-k8s-log.sh
+++ pravega-k8-log-collection/pravega-k8s-log.sh
@@ -8,15 +8,21 @@ for i in $(cat $output_dir/kubectl-get-po-svc-pvc-all-namespace_output | grep -i
 do
 echo $i
 if [[ $i == *"po/"* ]] || [[ $i == *"pod/"* ]]; then
-    if [ `echo $i | awk '{print $5}'` != 0 ]; then     
-        kubectl -n `echo $i | awk '{print $1}'` logs `echo $i | awk '{print $2}'` -p > $output_dir/log_`echo $i | awk 'gsub(/\//,"_") {print $2}'`_previous.log
+    if [ "`echo $i | awk '{print $4}'`" == "Running" ]; then
+    {
+        if [ `echo $i | awk '{print $5}'` != 0 ]; then     
+            kubectl -n `echo $i | awk '{print $1}'` logs `echo $i | awk '{print $2}'` -p > $output_dir/log_`echo $i | awk 'gsub(/\//,"_") {print $2}'`_previous.log
+        fi
+        kubectl -n `echo $i | awk '{print $1}'` logs `echo $i | awk '{print $2}'` > $output_dir/log_`echo $i | awk 'gsub(/\//,"_") {print $2}'`_recent.log
+        log_count=$(kubectl -n `echo $i | awk '{print $1}'` exec -it `echo $i |  awk '-F'[/] '{print $2}'|awk '{print $1}'` -- ls -l /opt/pravega/logs 2>/dev/null | wc -l || true)
+        if [ $log_count -ge 2 ]; then  
+            mkdir -p $output_dir/oldlogs_`echo $i |  awk '-F'[/] '{print $2}'|awk '{print $1}'` 
+            kubectl -n `echo $i | awk '{print $1}'` cp `echo $i |  awk '-F'[/] '{print $2}'|awk '{print $1}'`:/opt/pravega/logs $output_dir/oldlogs_`echo $i |  awk '-F'[/] '{print $2}'|awk '{print $1}'`/.
+        fi    
+    }
+    else
+       echo "`echo $i | awk '{print $2}'` is not in running state"  
     fi
-    kubectl -n `echo $i | awk '{print $1}'` logs `echo $i | awk '{print $2}'` > $output_dir/log_`echo $i | awk 'gsub(/\//,"_") {print $2}'`_recent.log
-    has_logs=$(kubectl -n `echo $i | awk '{print $1}'` exec -it `echo $i |  awk '-F'[/] '{print $2}'|awk '{print $1}'` -- ls -l /opt/pravega/logs 2>/dev/null | wc -l || echo "")
-    if [ $has_logs -ge 2 ]; then  
-        mkdir -p $output_dir/oldlogs_`echo $i |  awk '-F'[/] '{print $2}'|awk '{print $1}'` 
-        kubectl -n `echo $i | awk '{print $1}'` cp `echo $i |  awk '-F'[/] '{print $2}'|awk '{print $1}'`:/opt/pravega/logs $output_dir/oldlogs_`echo $i |  awk '-F'[/] '{print $2}'|awk '{print $1}'`/.
-    fi    
 fi
 kubectl -n `echo $i | awk '{print $1}'` describe `echo $i | awk '{print $2}'` > $output_dir/describe_`echo $i | awk 'gsub(/\//,"_") {print $2}'`_output
 done
