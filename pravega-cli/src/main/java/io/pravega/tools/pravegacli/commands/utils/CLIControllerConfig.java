d8b8777cd3d0c6ae9208b6a9415b6d8d0e171c8c
Raúl Gracia <raul.gracia@emc.com>
2019-05-28 08:52:25 +0200

    Issue 22: Controller describe-stream command bug (#29)
    Added storage backend type (segmentstore/zookeeper) option to perform queries for describe-stream command.
    
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>

--- pravega-cli/src/main/java/io/pravega/tools/pravegacli/commands/utils/CLIControllerConfig.java
+++ pravega-cli/src/main/java/io/pravega/tools/pravegacli/commands/utils/CLIControllerConfig.java
@@ -15,19 +15,41 @@ import io.pravega.common.util.Property;
 import io.pravega.common.util.TypedProperties;
 import lombok.Getter;
 
+/**
+ * Configuration for CLI client, specially related to the Controller service in Pravega.
+ */
 public final class CLIControllerConfig {
 
-    public static final Property<String> CONTROLLER_REST_URI = Property.named("controllerRestUri", "http://localhost:9091");
-    public static final Property<String> CONTROLLER_USER_NAME = Property.named("userName", "");
-    public static final Property<String> CONTROLLER_PASSWORD = Property.named("password", "");
+    public enum MetadataBackends {
+        SEGMENTSTORE, ZOOKEEPER
+    }
 
-    public static final String COMPONENT_CODE = "cli";
+    private static final Property<String> CONTROLLER_REST_URI = Property.named("controllerRestUri", "http://localhost:9091");
+    private static final Property<String> CONTROLLER_GRPC_URI = Property.named("controllerGrpcUri", "tcp://localhost:9090");
+    private static final Property<Boolean> AUTH_ENABLED = Property.named("authEnabled", false);
+    private static final Property<String> CONTROLLER_USER_NAME = Property.named("userName", "");
+    private static final Property<String> CONTROLLER_PASSWORD = Property.named("password", "");
+    private static final Property<String> METADATA_BACKEND = Property.named("metadataBackend", MetadataBackends.SEGMENTSTORE.name());
+
+    private static final String COMPONENT_CODE = "cli";
 
     /**
      * The Controller REST URI. Recall to set "http" or "https" depending on the TLS configuration of the Controller.
      */
     @Getter
     private final String controllerRestURI;
+
+    /**
+     * The Controller GRPC URI. Recall to set "tcp" or "tls" depending on the TLS configuration of the Controller.
+     */
+    @Getter
+    private final String controllerGrpcURI;
+
+    /**
+     * Defines whether or not to use authentication in Controller requests.
+     */
+    @Getter
+    private final boolean authEnabled;
 
     /**
      * User name if authentication is configured in the Controller.
@@ -41,10 +63,19 @@ public final class CLIControllerConfig {
     @Getter
     private final String password;
 
+    /**
+     * Controller metadata backend. At the moment, its values can only be "segmentstore" or "zookeeper".
+     */
+    @Getter
+    private final String metadataBackend;
+
     private CLIControllerConfig(TypedProperties properties) throws ConfigurationException {
         this.controllerRestURI = properties.get(CONTROLLER_REST_URI);
+        this.controllerGrpcURI = properties.get(CONTROLLER_GRPC_URI);
+        this.authEnabled = properties.getBoolean(AUTH_ENABLED);
         this.userName = properties.get(CONTROLLER_USER_NAME);
         this.password = properties.get(CONTROLLER_PASSWORD);
+        this.metadataBackend = properties.get(METADATA_BACKEND);
     }
 
     /**
